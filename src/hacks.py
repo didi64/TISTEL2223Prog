2c106b84e6b4f3c75a6b6638e3d35cab51bd22f9
Dieter Probst <dieter.probst@gmail.com>
2022-10-29 13:54:52 +0200

    .

--- src/hacks.py
+++ src/hacks.py
@@ -1,14 +1,14 @@
 import os 
 def fix_paths(paths):
-    '''paths is a list of paths below /home/jovyan/work/NIA22Prog/
+    '''paths is a list of paths below /home/jovyan/work/TISTEL2223Prog/
        returns relative paths form current working directory
        fix('images/str_methods_1.png') returns
        
        
        Example:
        fix(['images/str_methods_1.png']) returns
-         ['../../NIA22Prog/images/str_methods_3.png'] if called from work/notebooks/foo.ipynb
-         ['../NIA22Prog/images/str_methods_1.png']    if called from NIA22Prog/L3/foo.ipynb
+         ['../../TISTEL2223Prog/images/str_methods_3.png'] if called from work/notebooks/foo.ipynb
+         ['../TISTEL2223Prog/images/str_methods_1.png']    if called from TISTEL2223Prog/L3/foo.ipynb
     '''
     cdw = os.getcwd()[17:]
-    return [cdw.count('/') * '../' + 'NIA22Prog/' + path for path in paths]
+    return [cdw.count('/') * '../' + 'TISTEL2223Prog/' + path for path in paths]
