b651a1660445a5c0bce024e86fddac236e9d29cf
Dieter Probst <dieter.probst@gmail.com>
2022-11-05 20:59:12 +0100

    .

--- src/guessing_game.py
+++ src/guessing_game.py
@@ -7,25 +7,27 @@ def history2table(h):
        h: list of tuples, [(guess, comment),...]
     '''
     header = ('Nr.', 'guess', 'comment')
-    rows =  [(str(i), str(n), c) for i, (n, c) in enumerate(h)]    
+    rows =  [(i + 1, n, c) for i, (n, c) in enumerate(h)]    
     return table2str(header, rows)
 
-def eval_guess(guess, n):
-    '''returns a tuple (is_ok: bool, comment: str)
+def eval_guess(guess, nbr):
+    '''returns a tuple (is_ok: bool, comment: str) depending on whether
+       guess equals number, is too big or too small
     
        is_ok: True, if guess equals n
        comment: 'too big', 'correct' or 'too small'
     '''
+    
     comments = {1: 'too big', 
                 0: 'correct', 
                 -1: 'too small',
                }
     
-    is_ok = guess == n
-    key = sign(guess - n)
+    is_ok = guess == nbr
+    key = sign(guess - nbr)
     return is_ok, comments[key]
 
-def play_game(lower = 0, upper = 10):
+def play_game(lower = 1, upper = 10):
     '''guess a random Integer between lower and upper,
        lower and upper are included
     '''   
@@ -35,14 +37,15 @@ def play_game(lower = 0, upper = 10):
     is_correct = False
     while not is_correct:
         n = input('Zahl zwischen {a} und {b}? '.format(a=lower, b=upper))
-        n = int(n)
-        is_correct, comment = eval_guess(n, nbr)
-        history.append((n, comment))   
+        guess = int(n)
+        is_correct, comment = eval_guess(guess, nbr)
+        history.append((guess, comment))   
         
         print(comment)
         
     table = history2table(history)
-    print('Congrats! You guessed the number {N} in {n} tries.\n'.format(N=nbr, n=len(history)))
+    print('Congrats! You guessed the number {N} in {n} tries.\n'\
+          .format(N=nbr, n=len(history)))
     print(table)
     
     
--- src/toolbox.py
+++ src/toolbox.py
@@ -1,34 +1,7 @@
 '''
-Collection of usefull functions
-type 'help(toolbox)' for more help
+Collection of usefull functions.
+Type 'help(toolbox)' for more help.
 '''
-
-import ipywidgets as widgets
-
-class OutputWidget(widgets.Output):
-    def __init__(self):
-        super().__init__(layout = {'border': '1px solid black'})
-
-def is_number(x):
-    '''returns True if x is an int or a float'''
-    try:
-        int(x)
-    except ValueError:
-        try:
-            float(x)
-        except ValueError:
-            return False
-    return True    
-
-def str2number(x):
-    '''returns int(x) or float(x) or x'''
-    if not is_number(x):
-        return x
-    try:
-        int(x)
-    except ValueError:
-        return  float(x)
-    return int(x)
 
 def sign(x):
     '''returns the sign of the number x'''
@@ -37,27 +10,29 @@ def sign(x):
 def table2str(header, rows, sep = ' │ ', fillchar = ' ', hline_char = '━'):
     '''returns a string displaying a table
     
-       header: tuple of column names or numbers, 
-       e.g. ('Nr.', 'Name', 'Email')
-       rows:  list of tuples of strings or numbers, 
-       e.g. [(1, 'Bob', 'foo@bar.ch'), ...]  
+       header: tuple[int, float, str], tuple of column names, 
+         e.g. ('Nr.', 'Name', 'Email')
+       rows: list[tuple[int, float, str]], list of table row entries, 
+         e.g. [(1, 'Bob', 'foo@bar.ch'), ...]
     '''   
+    
     def row2str(row, widths, sep, fillchar):
-        fstring = (sep+'{}') * len(row) + sep
+        fstring = (sep + '{}') * len(row) + sep
         args = []
-        for s, w in zip(row, widths):
-            if type(s) == int or type(s) == float:
-                s = str(s).rjust(w, fillchar)
+        for item, width in zip(row, widths):
+            if isinstance(item, (int, float)):
+                item = str(item).rjust(width, fillchar)
             else:    
-                s = str(s).ljust(w, fillchar)
-            args.append(s)
+                item = str(item).ljust(width, fillchar)
+                
+            args.append(item)
    
         return fstring.format(*args)
 
     def get_widths(header, rows):
         widths = [len(h) for h in header]
         for row in rows:
-            for i,item in enumerate(row):
+            for i, item in enumerate(row):
                 widths[i] = max(widths[i], len(str(item)))
         return widths                  
 
@@ -71,5 +46,4 @@ def table2str(header, rows, sep = ' │ ', fillchar = ' ', hline_char = '━'):
     for row in rows:
         res.append(row2str(row, widths, sep, fillchar))
         
-    return '\n'.join(res)       
-    
+    return '\n'.join(res)
